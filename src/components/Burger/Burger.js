ffb9a79bf51699c3670d6b8745e2e39594c5be56
Dmitrii Shatov <Dmitrii.Sh86@yandex.ru>
2023-09-17 21:10:51 +0300

    init

--- src/components/Burger/Burger.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Burger({themeBurger, openPopup}) {
-    return (
-        <button onClick={openPopup} className={`burger__container ${themeBurger}`}></button>
-    );
-}
-
-export default Burger;
